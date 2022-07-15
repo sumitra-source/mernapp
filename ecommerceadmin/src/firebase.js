ccac855add34becbf080d2352164bb20991949d9
sumitra <sumitraashrestha@gmail.com>
2022-07-15 08:38:46 +0545

    finalproject

---
+++ ecommerceadmin/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmOIErRzr2lbBuUCR97fugBnFDOUWvI8s",
+  authDomain: "shop-db109.firebaseapp.com",
+  projectId: "shop-db109",
+  storageBucket: "shop-db109.appspot.com",
+  messagingSenderId: "877325570171",
+  appId: "1:877325570171:web:6deb00fad38aa6b2ac92de",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
